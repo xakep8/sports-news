e3ab5b5f1ea0849f93ebe3ce767a824796659ab1
Kunal Dubey <xakep8@protonmail.com>
2023-11-11 20:53:03 +0530

    minor changes

--- src/layouts/account/Appbar.tsx
+++ src/layouts/account/Appbar.tsx
@@ -4,13 +4,13 @@ import { UserCircleIcon } from '@heroicons/react/24/outline'
 import Logo from "../../assets/logo-removebg-preview.png"
 import { Link, useLocation } from "react-router-dom"
 import { Disclosure, Menu, Transition, Switch } from '@headlessui/react';
-import { useState,useContext } from 'react';
+import { useState, useContext } from 'react';
 import { ThemeContext } from '../../context/theme';
 import PreferencesDialog from '../../components/preferencesDialog';
 
 const userNavigation = [
-  {name:'Reset Password',href:'/reset'},
-  {name:'Logout',href:'/logout'},
+  { name: 'Reset Password', href: '/reset' },
+  { name: 'Logout', href: '/logout' },
 ]
 
 const classNames = (...classes: string[]): string => classes.filter(Boolean).join(' ');
@@ -18,8 +18,8 @@ const classNames = (...classes: string[]): string => classes.filter(Boolean).joi
 const Appbar = () => {
   const { theme, setTheme } = useContext(ThemeContext);
   const [enabled, setEnabled] = useState(true);
-  const [isAuth,setAuth] = useState(false);
-  const [isOpen,setIsOpen]=useState(false);
+  const [isAuth, setAuth] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleTheme = () => {
     let newTheme = ''
@@ -39,7 +39,7 @@ const Appbar = () => {
     { name: 'SignUp', href: '/signup', current: false },
   ]
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     setAuth(!!localStorage.getItem("authToken"));
   });
 
@@ -62,33 +62,34 @@ const Appbar = () => {
               </div>
               <div className="hidden md:block">
                 <div className="ml-4 inline-flex items-center md:ml-6">
-                {isAuth?
-                <div></div>
-                :
-                <div className="hidden md:block">
-                  <div className="ml-10 flex items-end space-x-4">
-                    {navigation.map((item) => { 
-                      const isCurrent = pathname.includes(item.href)
+                  {isAuth ?
+                    <div></div>
+                    :
+                    <div className="hidden md:block">
+                      <div className="ml-10 flex items-end space-x-4">
+                        {navigation.map((item) => {
+                          const isCurrent = pathname.includes(item.href)
 
-                      return (
-                        <Link
-                          key={item.name}
-                          to={item.href}
-                          className={classNames(
-                            isCurrent
-                              ? 'bg-slate-50 text-blue-700'
-                              : 'text-slate-500 hover:text-blue-600',
-                            'rounded-md px-3 py-2 text-sm font-medium'
-                          )}
-                          aria-current={isCurrent ? 'page' : undefined}
-                        >
-                          {item.name}
-                        </Link>
-                    )})}
-                  </div>
-                </div>
-                }
-                <Switch
+                          return (
+                            <Link
+                              key={item.name}
+                              to={item.href}
+                              className={classNames(
+                                isCurrent
+                                  ? 'bg-slate-50 text-blue-700'
+                                  : 'text-slate-500 hover:text-blue-600',
+                                'rounded-md px-3 py-2 text-sm font-medium'
+                              )}
+                              aria-current={isCurrent ? 'page' : undefined}
+                            >
+                              {item.name}
+                            </Link>
+                          )
+                        })}
+                      </div>
+                    </div>
+                  }
+                  <Switch
                     checked={enabled}
                     onChange={toggleTheme}
                     className={`${enabled ? 'bg-slate-400' : 'bg-slate-700'}
@@ -100,60 +101,60 @@ const Appbar = () => {
                         pointer-events-none inline-block h-[16px] w-[16px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
                     />
                   </Switch>
-                  {isAuth?
+                  {isAuth ?
                     <Menu as="div" className="relative ml-3">
-                    <div>
-                      <Menu.Button className="rounded-full bg-white p-1 text-gray-400 hover:text-blue-600">
-                        <UserCircleIcon className="h-6 w-6" aria-hidden="true" />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item key={'preferences'}>
-                          {({active})=>(
-                            <button onClick={()=>setIsOpen(true)} className={classNames(active ? 'bg-gray-100' : '','block px-4 py-2 text-sm text-gray-700 w-full bg-white')}>
-                              Preferences
-                            </button>
-                          )}
-                        </Menu.Item>
-                        {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
+                      <div>
+                        <Menu.Button className="rounded-full bg-white p-1 text-gray-400 hover:text-blue-600">
+                          <UserCircleIcon className="h-6 w-6" aria-hidden="true" />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          <Menu.Item key={'preferences'}>
                             {({ active }) => (
-                              <Link
-                              key={item.name}
-                              to={item.href}
-                              className={
-                                'block text-sm text-gray-700 hover:text-gray-700'
-                              }
-                            >
-                              <button className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700 w-full bg-white'
-                              )}>
-                                  {item.name}
+                              <button onClick={() => setIsOpen(true)} className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 w-full bg-white')}>
+                                Preferences
                               </button>
-                              </Link>
                             )}
                           </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                  :
-                  <div></div>
+                          {userNavigation.map((item) => (
+                            <Menu.Item key={item.name}>
+                              {({ active }) => (
+                                <Link
+                                  key={item.name}
+                                  to={item.href}
+                                  className={
+                                    'block text-sm text-gray-700 hover:text-gray-700'
+                                  }
+                                >
+                                  <button className={classNames(
+                                    active ? 'bg-gray-100' : '',
+                                    'block px-4 py-2 text-sm text-gray-700 w-full bg-white'
+                                  )}>
+                                    {item.name}
+                                  </button>
+                                </Link>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                    :
+                    <div></div>
                   }
                 </div>
               </div>
             </div>
-            <PreferencesDialog open={isOpen} setOpen={setIsOpen}/>
+            <PreferencesDialog open={isOpen} setOpen={setIsOpen} />
           </div>
         )}
       </Disclosure>
