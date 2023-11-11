8d7f32f35af2caaa7632f3959e5b9d2a28a149de
Kunal Dubey <xakep8@protonmail.com>
2023-11-11 20:47:04 +0530

    change anchor to link

--- src/layouts/account/Appbar.tsx
+++ src/layouts/account/Appbar.tsx
@@ -131,14 +131,15 @@ const Appbar = () => {
                                 active ? 'bg-gray-100' : '',
                                 'block px-4 py-2 text-sm text-gray-700 w-full bg-white'
                               )}>
-                                <a
-                                  href={item.href}
+                                <Link
+                                  key={item.name}
+                                  to={item.href}
                                   className={
                                     'block text-sm text-gray-700 hover:text-gray-700'
                                   }
                                 >
                                   {item.name}
-                                </a>
+                                </Link>
                               </button>
                             )}
                           </Menu.Item>
